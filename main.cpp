f31864188b09ec4b937e47e0b8cab39baea6ca85
1234445666666 <p.k.270607@gmail.com>
2026-09-16 00:09:50 +0300

    fix code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 class Tasks
 {
+
 public:
     unsigned id{};
     std::string name{};
@@ -21,33 +22,64 @@ public:
     };
 };
 
-void hello_fn()
+enum class Text
 {
-    std::cout << "Это todo list." << std::endl;
-}
+    hello,
+    menu,
+    interface,
+    addTask,
+    inputTask,
+    inputId,
+    deleteTask,
+    fixTask,
+    notId,
+    notTask,
+    notFound
+};
 
-void setting_fn()
+std::string get_message(Text msg)
 {
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "Если хотите добавить задачу нажмите 1\nЕсли хотите удалить нажмите 2\nЕсли хотите исправить задачу нажмите 3\nЕсли хотите увидеть весь список задач нажмите 4\nЕсли хотите выйти нажмите 5. " << std::endl;
-    std::cout << "-----------------------------" << std::endl;
-}
-
-void complited_fn()
-{
-    std::cout << "Задача добавлена" << std::endl;
-}
+    switch (msg)
+    {
+    case Text::hello:
+        return "Это todo list.";
+    case Text::menu:
+        return "Добавить задачу - 1\nУдалить - 2\nИзменить задачу - 3\nВесь список задач - 4\nВыйти - 5.";
+    case Text::interface:
+        return "-----------------------------";
+    case Text::addTask:
+        return "Задача добавлена";
+    case Text::inputTask:
+        return "Напишите название задачи";
+    case Text::inputId:
+        return "Выберите id задачи";
+    case Text::deleteTask:
+        return "Задача удалена";
+    case Text::notId:
+        return "id не найден";
+    case Text::notTask:
+        return "Задача не найдена";
+    case Text::fixTask:
+        return "Напишите новое название задачи";
+    case Text::notFound:
+        return "Такой команды нет";
+    }
+    return "";
+};
 
 int main()
 {
     bool run = true;
     std::string task_name;
     std::vector<Tasks> tasks;
-    hello_fn();
+    std::cout << get_message(Text::hello) << std::endl;
     std::ofstream file("todo.txt", std::ios_base::out);
+    bool found = false;
     while (run)
     {
-        setting_fn();
+        std::cout << get_message(Text::interface) << std::endl;
+        std::cout << get_message(Text::menu) << std::endl;
+        std::cout << get_message(Text::interface) << std::endl;
 
         std::string response;
         std::cin >> response;
@@ -56,11 +88,11 @@ int main()
         {
         case 1:
         {
-            std::cout << "Напишите название задачи" << std::endl;
+            std::cout << get_message(Text::inputTask) << std::endl;
             std::cin >> task_name;
             Tasks newTasks(tasks.size() + 1, task_name, false);
             tasks.push_back(newTasks);
-            complited_fn();
+            std::cout << get_message(Text::addTask) << std::endl;
             break;
         }
         case 2:
@@ -68,7 +100,7 @@ int main()
             if (!tasks.empty())
             {
                 std::string edit_id;
-                std::cout << "Выберите id задачи" << std::endl;
+                std::cout << get_message(Text::inputId) << std::endl;
                 std::cin >> edit_id;
 
                 for (int i = 0; i < tasks.size(); i++)
@@ -76,18 +108,20 @@ int main()
                     if (tasks[i].id == std::stoi(edit_id))
                     {
                         tasks.erase(tasks.begin() + i);
-                        std::cout << "Задача удалена" << std::endl;
+                        std::cout << get_message(Text::deleteTask) << std::endl;
+                        found = true;
                         break;
                     }
                     else
                     {
-                        std::cout << "id не найден" << std::endl;
+                        std::cout << get_message(Text::notId) << std::endl;
+                        !found
                     }
                 }
             }
             else
             {
-                std::cout << "Задача не найдена" << std::endl;
+                std::cout << get_message(Text::notTask) << std::endl;
             }
             break;
         }
@@ -95,9 +129,9 @@ int main()
         {
             std::string rename;
             std::string edit_id;
-            std::cout << "Выберите id задачи" << std::endl;
+            std::cout << get_message(Text::inputId) << std::endl;
             std::cin >> edit_id;
-            std::cout << "Напишите новое название задачи" << std::endl;
+            std::cout << get_message(Text::fixTask) << std::endl;
             std::cin >> rename;
             for (int i = 0; i < tasks.size(); i++)
             {
@@ -131,10 +165,7 @@ int main()
             {
                 for (auto task : tasks)
                 {
-                    std::string complited = (task.isComplited == true ? "Сделанна" : "Не сделанна");
-                    file << "id: " << task.id;
-                    file << "Задача: " << task.name;
-                    file << " " << complited;
+                    file << task.id << ";" << task.name << ";" << (task.isComplited ? 1 : 0) << std::endl;
                 }
                 file.close();
             }
@@ -143,7 +174,7 @@ int main()
         }
 
         default:
-            std::cout << "Такой команды нет" << std::endl;
+            std::cout << get_message(Text::notFound) << std::endl;
             break;
         };
     }
